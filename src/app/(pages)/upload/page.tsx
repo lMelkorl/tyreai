36e52f2af88c60759b4a611a743378bfc447e63a
muhammedensari <muhammed.ensari@lbbilisim.com>
2023-11-09 09:54:27 +0300

    ds

--- src/app/(pages)/upload/page.tsx
+++ src/app/(pages)/upload/page.tsx
@@ -1,42 +1,51 @@
-"use client"
-import ImageClassification from '@/app/components/imageClassification/imageClassification'
-import { Image } from '@nextui-org/react'
-import React from 'react'
-import usingImage from "./using.png";
+// "use client"
+// import ImageClassification from '@/app/components/imageClassification/imageClassification'
+// import { Image } from '@nextui-org/react'
+// import React from 'react'
+// import usingImage from "./using.png";
 
-const Upload = () => {
-  const using = usingImage.src;
-  return (
-    <div className='container mx-auto mt-20'>
-         <div className="mb-10 ">
-        <div className="container px-20 mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2 flex items-center w-full">
-          <div className="col-span-2 sm:col-span-1 ">
-            <h2 className='text-4xl font-extrabold mb-6 mt-6 text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-              Hemen Deneyin
-            </h2>
-            <div className="col-span-2 sm:col-span-1 text-base text-lg dark:text-gray-400">
-              Örnekteki gibi lastiğinizin yüzey kısmının resmini çekerek yükleyebilirsiniz,unutmayın kullanılan mevcut model geliştirme aşamasında olup %86 doğruluk payı içeren bir yapay zeka modelidir.
-                 </div>
-          </div>
+// const Upload = () => {
+//   const using = usingImage.src;
+//   return (
+//     <div className='container mx-auto mt-20'>
+//          <div className="mb-10 ">
+//         <div className="container px-20 mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2 flex items-center w-full">
+//           <div className="col-span-2 sm:col-span-1 ">
+//             <h2 className='text-4xl font-extrabold mb-6 mt-6 text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+//               Hemen Deneyin
+//             </h2>
+//             <div className="col-span-2 sm:col-span-1 text-base text-lg dark:text-gray-400">
+//               Örnekteki gibi lastiğinizin yüzey kısmının resmini çekerek yükleyebilirsiniz,unutmayın kullanılan mevcut model geliştirme aşamasında olup %86 doğruluk payı içeren bir yapay zeka modelidir.
+//                  </div>
+//           </div>
     
 
-          <div className="col-span-2 sm:col-span-1 flex justify-end px-10">
-          <Image
-                width={200}
-                height={200}
-                className="rounded "
-                src={using}
-                alt=""
-              />
-          </div>
-        </div>
-      </div>
+//           <div className="col-span-2 sm:col-span-1 flex justify-end px-10">
+//           <Image
+//                 width={200}
+//                 height={200}
+//                 className="rounded "
+//                 src={using}
+//                 alt=""
+//               />
+//           </div>
+//         </div>
+//       </div>
 
 
-      <div className="mt-20 mb-20">
-      <ImageClassification/>
-      </div>
-    </div>
+//       <div className="mt-20 mb-20">
+//       <ImageClassification/>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Upload
+
+
+const Upload=()=>{
+  return (
+    <div>aa</div>
   )
 }
 
